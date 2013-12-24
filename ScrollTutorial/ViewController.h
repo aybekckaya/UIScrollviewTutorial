b01f76d611bda6b80d7c55ce5c7ac29719fd85de
aybekckaya <aybekcankaya@icloud.com>
2013-12-24 19:11:40 +0200

    Initial Commit

---
+++ ScrollTutorial/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ScrollTutorial
+//
+//  Created by aybek can kaya on 12/24/13.
+//  Copyright (c) 2013 aybek can kaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ScrollTutorial/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ScrollTutorial
+//
+//  Created by aybek can kaya on 12/24/13.
+//  Copyright (c) 2013 aybek can kaya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
